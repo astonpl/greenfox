14aedca76d289bb7e1047a2e0b1c9230181bcfb5
astonpl <aston@gg.pl>
2014-04-15 13:32:34 +0200

    Postęp prac

--- _system/init_mysql.php
+++ _system/init_mysql.php
@@ -6,21 +6,37 @@
 		private $_prefix;
 		
 		public function __construct($host,$username,$password,$dbname){
-			//@mysql_connect($host,$username,$password) or die(''); //ToDoIt:	
-			//@mysql_select_db($dbname) or die(''); //ToDoIt:
+			@mysql_connect($host,$username,$password) or $this->_connected=false; 
+			@mysql_select_db($dbname) or $this->_connected=false; 
+			//ToDoIt:
 		}
 		
-		public function parseSQL($mask){
-			$numargs = func_num_args();
-			$arg_list = func_get_args();
-			for($i=1;$i<$numargs;$i++){
-				$mask= str_replace('%'.$i,addslashes($arg_list[$i]),$mask);
+		public function isConnected(){
+			return $this->_connected;
+		}
+		
+		public function parseSQL($mask,$arg_list){
+			for($i=0;$i<count($arg_list);$i++){
+				$mask= str_replace('%'.($i+1),addslashes($arg_list[$i]),$mask);
 			}
-			print $mask;
+			print $mask; //ToDoIt:
 		}
 		
 	}
 	
+	function sqlQ($mask){
+		global $_system_mysql;
+		$arg_list = func_get_args();
+		$num      = func_num_args();
+		$ar = array();
+		for($i=1;$i<$num;$i++){
+			$ar[] = $arg_list[$i];
+		}
+		print_r($ar); //ToDoIt:
+		return $_system_mysql->parseSQL($mask,$ar);
+	}
+	
 	$_system_mysql = new TMysql(cfg('mysql','host'),cfg('mysql','username'),cfg('mysql','password'),cfg('mysql','dbname'));
-	$_system_mysql->parseSQL("SELECT * FROM `%1` WHERE `id`='%2'","test",15);
+	sqlQ("SELECT * FROM `%1` WHERE `id`='%2'","test",15);
+	var_dump($_system_mysql->isConnected());
 ?>
